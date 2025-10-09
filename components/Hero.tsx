3e87a07600d6f61f2fec6e8efb85c6615960f39d
Jalpa Rai <97447460+jalparai@users.noreply.github.com>
2025-10-10 03:18:33 +0500

    code updates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,10 +33,10 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Designing enterprise-level Saas platform
+            Let's Connect 🟢 Open to work
           </p>
           <TextGenerateEffect
-            words="Design Hight "
+            words="Designing enterprise-level Saas platform "
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
